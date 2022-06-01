7e85b14f9241d35d0c360861d5acabb654332a75
Kayin Ilagan <kayinilagan@gmail.com>
2022-05-31 21:12:39 -0400

    Added Dice Images, coded functionality of App and created a Unit Test for the dice roll() function.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 // Imports required for program use
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
         // Get the Button on Main View and have the button roll dice onClick
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        //Roll dice when Application starts
+        rollDice()
     }
 
 
@@ -21,6 +25,20 @@ class MainActivity : AppCompatActivity() {
         // Editable number of sides
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        // Get Image View
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // Get Appropriate Dice Image Drawable Resource based on result of diceRoll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // Set Image from Drawable Resource and set contentDescription for Accessibility
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
